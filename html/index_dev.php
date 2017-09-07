bc9cb15b73529e1129fe5439f695e3cebfa485c0
wangrunbo <wangrunbo921@gmail.com>
2017-09-07 17:14:30 +0900

    お問い合わせリンク

--- html/index_dev.php
+++ html/index_dev.php
@@ -37,7 +37,7 @@ $allow = array(
 
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], $allow)
+//    || !in_array(@$_SERVER['REMOTE_ADDR'], $allow)
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
